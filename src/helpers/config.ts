4db7e698e48b5ad144ce44d668e996004e212c5e
shubham <shubham.narang@decisionpoint.in>
2021-07-06 17:57:42 +0530

    removes db creds

--- src/helpers/config.ts
+++ src/helpers/config.ts
@@ -1,11 +1,11 @@
 export const CONFIG = {
   PORT: 4002,
   DATABASE: {
-    HOST: 'sql6.freesqldatabase.com',
+    HOST: '',
     PORT: 3306,
-    DATABASE: 'sql6423108',
-    USERNAME: 'sql6423108',
-    PASSEORD: 'jqgFYWtvCf',
+    DATABASE: '',
+    USERNAME: '',
+    PASSEORD: '',
     DIALECT: 'mysql',
   },
 };
